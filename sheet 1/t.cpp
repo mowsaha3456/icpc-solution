008aedfa68815987e73d94a25be0c63098bc262c
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:02:38 +0600

    Update t.cpp

--- sheet 1/t.cpp
+++ sheet 1/t.cpp
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+ 
+int main() {
+    float N;
+    scanf("%f", &N);
+ 
+    int intPart = (int)N;
+    float decPart = N - intPart;
+ 
+    if (decPart == 0)
+        printf("int %d\n", intPart);
+    else
+        printf("float %d %.3f\n", intPart, decPart);
+ 
+    return 0;
+}

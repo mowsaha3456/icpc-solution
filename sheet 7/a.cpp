8da438e9c52205bd12960ce251c321ff2a7e964f
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 19:02:51 +0600

    Update a.cpp

--- sheet 7/a.cpp
+++ sheet 7/a.cpp
@@ -1,1 +1,28 @@
+#include<stdio.h>
+ 
+void solve(int i,int n)
+ 
+{
+ 
+if(i<=n) {
+ 
+printf("I love Recursion\n");
+ 
+solve(i+1,n);
+ 
+}
+ 
+}
+ 
+int main()
+ 
+{
+ 
+int n;
+ 
+scanf("%d",&n);
+ 
+solve (1,n);
+ 
+}
 

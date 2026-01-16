c1b90d690bc6d482f2fedd983cc011eee1f50632
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:55:16 +0600

    Update L. New Array.c

--- sheet 5 (Functions)/L. New Array.c
+++ sheet 5 (Functions)/L. New Array.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+ 
+void mow() {
+    int n;
+    scanf("%d", &n);
+ 
+    int A[n], B[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &A[i]);
+ 
+    for(int i = 0; i < n; i++)
+        scanf("%d", &B[i]);
+ 
+    for(int i = 0; i < n; i++)
+        printf("%d ", B[i]);
+ 
+    for(int i = 0; i < n; i++)
+        printf("%d ", A[i]);
+}
+ 
+int main() {
+    mow();
+    
+}

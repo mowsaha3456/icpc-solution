7598187c1a1621bceb05299a6a3b1cef072658f9
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:35:17 +0600

    Update solve G.c

--- sheet 4  (Strings)/solve G.c
+++ sheet 4  (Strings)/solve G.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include <string.h>
+int main(){
+   char d[100005];
+  scanf("%s",d);
+   int i=0;
+ 
+   for(i=0;i<strlen(d);i++){
+    if(d[i]==','){
+        d[i]=' ';}
+ 
+ 
+   else if  (d[i] >= 'a' && d[i] <= 'z'){
+    d[i] = d[i]-32;
+   }
+ else if (d[i] >= 'A' && d[i] <= 'Z'){
+    d[i] = d[i]+32;}
+ 
+   }
+   printf("%s\n",d);
+}

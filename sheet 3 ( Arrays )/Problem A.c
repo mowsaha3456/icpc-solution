272e4941d99407f5ec22fbb95c4ed9f68cba3572
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:23:35 +0600

    Update Problem A.c

--- sheet 3 ( Arrays )/Problem A.c
+++ sheet 3 ( Arrays )/Problem A.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main() {
+    int N, x;
+    long long sum = 0;
+ 
+    scanf("%d", &N);
+ 
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &x);
+        sum += x;
+    }
+ 
+    if (sum < 0)
+        sum = -sum;
+ 
+    printf("%lld\n", sum);
+ 
+ 
+}

cd47b27ac14520fc20811acbb8dd688a0de7a063
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:51:01 +0600

    Update B. Print.c

--- sheet 5 (Functions)/B. Print.c
+++ sheet 5 (Functions)/B. Print.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+ 
+void mow()
+{
+    int n, i;
+    scanf("%d", &n);
+ 
+    for(i = 1; i <= n; i++)
+    {
+        if(i > 1)
+            printf(" ");
+        printf("%d", i);
+    }
+}
+ 
+int main()
+{
+    mow();
+    
+}
 

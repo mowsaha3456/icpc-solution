b76230075a6f8576e2f1df2473921104834a73ba
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:53:09 +0600

    Update G. Max and MIN.c

--- sheet 5 (Functions)/G. Max and MIN.c
+++ sheet 5 (Functions)/G. Max and MIN.c
@@ -1,1 +1,32 @@
-
+#include <stdio.h>
+ 
+void minMax()
+{
+    int n, i;
+    int a[1000];
+    int min, max;
+ 
+    scanf("%d", &n);
+ 
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+ 
+    min = a[0];
+    max = a[0];
+ 
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] < min)
+            min = a[i];
+        if(a[i] > max)
+            max = a[i];
+    }
+ 
+    printf("%d %d", min, max);
+}
+ 
+int main()
+{
+    minMax();
+    
+}

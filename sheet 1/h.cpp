8fd02a7a2df53d0b1c86b68831da94503c9c5047
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 17:47:02 +0600

    Update h.cpp

--- sheet 1/h.cpp
+++ sheet 1/h.cpp
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+    double A, B;
+    scanf("%lf %lf", &A, &B);
+ 
+    printf("floor %.0lf / %.0lf = %.0lf\n", A, B, floor(A / B));
+    printf("ceil %.0lf / %.0lf = %.0lf\n", A, B, ceil(A / B));
+    printf("round %.0lf / %.0lf = %.0lf\n", A, B, round(A / B));
+ 
+    return 0;
+}

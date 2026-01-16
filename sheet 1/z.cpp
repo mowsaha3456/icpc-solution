c36088b3af439597a95ce04ce86dd4c9dfc29e03
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:03:53 +0600

    Update z.cpp

--- sheet 1/z.cpp
+++ sheet 1/z.cpp
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+    double A, B, C, D;
+    scanf("%lf %lf %lf %lf", &A, &B, &C, &D);
+ 
+    // Compare A^B and C^D without overflow using logarithms
+    double left = B * log(A);
+    double right = D * log(C);
+ 
+    if (left > right)
+        printf("YES\n");
+    else
+        printf("NO\n");
+ 
+    return 0;
+}

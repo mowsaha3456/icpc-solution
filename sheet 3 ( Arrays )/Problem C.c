f5568ee17be079989cb753f763e254fca8e33617
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:24:53 +0600

    Update Problem C.c

--- sheet 3 ( Arrays )/Problem C.c
+++ sheet 3 ( Arrays )/Problem C.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int mow[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&mow[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(mow[i] > 0)
+            printf("1 ");
+        else if (mow[i] < 0)
+            printf("2 ");
+        else
+            printf("0 ");
+    }
+}
 

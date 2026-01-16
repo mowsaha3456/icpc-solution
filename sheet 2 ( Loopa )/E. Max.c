906fa954825036f5e221e60cc7df282deb5660bb
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:16:44 +0600

    Update E. Max.c

--- sheet 2 ( Loopa )/E. Max.c
+++ sheet 2 ( Loopa )/E. Max.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main ()
+{
+    int x,n,max=-999999;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&x);
+        if(x>max)
+            max=x;
+ 
+    }
+    printf("%d",max);
+    }
+ 
+ 
+ 
+ 

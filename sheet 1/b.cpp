a6599a48541fc2d230b9bff1694989511b18200a
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 17:38:52 +0600

    Update b.cpp

--- sheet 1/b.cpp
+++ sheet 1/b.cpp
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+ 
+int main() {
+    int i;
+    long long int a;
+    char ch;
+    float f;
+    double d;
+ 
+    scanf("%d %lld %c %f %lf",&i,&a,&ch,&f,&d);
+    printf("%d\n%lld\n%c\n%f\n%lf\n",i,a,ch,f,d);
+ 
+    return  0;
+ 
+ 
+}

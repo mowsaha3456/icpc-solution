ccd041cd189a6c02dfdcb0357b53ce602f530d20
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:00:55 +0600

    Update m.cpp

--- sheet 1/m.cpp
+++ sheet 1/m.cpp
@@ -1,1 +1,15 @@
 
+#include<stdio.h>
+int main()
+{char x;
+scanf("%c",&x);
+if(x >= 'A'&&x <= 'Z'){
+    printf("ALPHA\nIS CAPITAL");
+}
+else if (x >= '0' && x <= '9'){
+    printf("IS DIGIT");
+}
+else if (x >= 'a' && x <='z'){
+    printf("ALPHA\nIS SMALL");
+}
+}

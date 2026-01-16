0dae7ce9a6a1b335c714fc85728941e2dfb6d9ca
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:16:11 +0600

    Update D. Fixed Password.c

--- sheet 2 ( Loopa )/D. Fixed Password.c
+++ sheet 2 ( Loopa )/D. Fixed Password.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int x;
+    while(1){
+    scanf("%d",&x);
+    if(x==1999){
+        printf("Correct\n");
+        break;
+ 
+    }else{
+        printf("Wrong\n");
+}
+    }
+ 
+ 
+ 
+}
 

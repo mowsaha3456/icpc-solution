bef09c36da4326b37d95bd390575c2d84c86a134
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:36:35 +0600

    Update solve I.c

--- sheet 4  (Strings)/solve I.c
+++ sheet 4  (Strings)/solve I.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+#include <string.h>
+int main(){
+   char s[1003];
+   scanf("%s",s);
+   int n=strlen(s);
+   int first = 0;
+   int last = n-1;
+   while(first<last){
+    if(s[first]!= s[last]){
+        printf("NO\n");
+        return 0;
+ 
+    }
+    first++;
+    last--;
+   } printf("YES\n");
+}

2598da3879d457f2e466603f88565c9043bc2c52
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:15:33 +0600

    Update C. Even, Odd, Positive and Negative.c

--- sheet 2 ( Loopa )/C. Even, Odd, Positive and Negative.c
+++ sheet 2 ( Loopa )/C. Even, Odd, Positive and Negative.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+int main ()
+{
+    int n,x;
+    int pos=0,neg=0,even =0,odd=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+            scanf("%d",&x);
+    if(x%2==0)
+        even++;
+        if(x>0)
+        pos++;
+        if(x<0)
+        neg++;}{
+ 
+      printf("Even: %d\n",even);
+      printf("Odd: %d\n",n-even);
+      printf("Positive: %d\n",pos);
+      printf("Negative: %d\n",neg);
+ 
+ 
+ 
+        }
+}

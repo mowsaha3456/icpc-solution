7488a38c7a2cfc299552245da757fff5b39e4be3
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 17:40:45 +0600

    Update c.cpp

--- sheet 1/c.cpp
+++ sheet 1/c.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    long long int x , y;
+    scanf("%lld %lld",&x,&y);
+    printf("%lld + %lld = %lld\n",x,y,x+ y);
+    printf("%lld * %lld = %lld\n",x,y,x* y);
+    printf("%lld - %lld = %lld\n",x,y,x- y);
+ 
+    return 0;
+ 
+}
 

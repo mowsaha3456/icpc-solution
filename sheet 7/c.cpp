247f285e5473538e48bb60e3eebc3d51907a4549
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 19:04:25 +0600

    Update c.cpp

--- sheet 7/c.cpp
+++ sheet 7/c.cpp
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+void mow(int i){
+    if(i>=1){
+   printf("%d",i);
+   if(i>1)
+   printf(" ");
+   mow(i-1);}
+}
+   int main (){
+       int n;
+       scanf("%d",&n);
+    mow(n);
+    }

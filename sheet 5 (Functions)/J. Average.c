caad2c2f144250c210226ee7b76d16e14ae4fdec
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:53:49 +0600

    Update J. Average.c

--- sheet 5 (Functions)/J. Average.c
+++ sheet 5 (Functions)/J. Average.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+ 
+void average(double a[], int n)
+{
+    int i;
+    double sum = 0;
+ 
+    for(i = 0; i < n; i++)
+        sum = sum + a[i];
+ 
+    printf("%.7lf", sum / n);
+}
+ 
+int main()
+{
+    int n, i;
+    double a[10000];
+ 
+    scanf("%d", &n);
+ 
+    for(i = 0; i < n; i++)
+        scanf("%lf", &a[i]);
+ 
+    average(a, n);
+    return 0;
+}

44ada130dea8e67bf941b02e8751d363c49eb157
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:24:19 +0600

    Update Problem B.c

--- sheet 3 ( Arrays )/Problem B.c
+++ sheet 3 ( Arrays )/Problem B.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+ 
+int main() {
+    int N;
+    scanf("%d", &N);
+ 
+    long long A[N], X;
+    for(int i=0;i<N;i++) scanf("%lld",&A[i]);
+    scanf("%lld",&X);
+ 
+    int found = -1;
+    for(int i=0;i<N;i++){
+        if(A[i] == X){
+            found = i;
+            break;
+        }
+    }
+    printf("%d\n", found);
+    return 0;
+}

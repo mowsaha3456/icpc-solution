d2fa564e815b81134b4cc787a3438c15c39089a8
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:50:25 +0600

    Update A. Add.c

--- sheet 5 (Functions)/A. Add.c
+++ sheet 5 (Functions)/A. Add.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+ 
+void findSum()
+{
+    int a,b;
+    scanf("%d %d", &a, &b);
+    int x= a+b;
+    printf("%d", x);
+}
+ 
+int main()
+{
+    findSum();
+    return 0;
+}
+
+
 

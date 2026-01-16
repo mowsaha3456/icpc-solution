5c6584bd099d7d9129fcbc25187b9b08f27011c1
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 18:26:09 +0600

    Update Problem S.c

--- sheet 3 ( Arrays )/Problem S.c
+++ sheet 3 ( Arrays )/Problem S.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,x;
+    scanf("%d %d",&n,&m);
+    int mow[n][m];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            scanf("%d",&mow[i][j]);
+        }
+    }
+    scanf("%d",&x);
+    int found =0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(mow[i][j]==x)
+            {
+                found = 1;
+                break;
+            }
+        }
+    }
+    if(found==0)
+        printf("will take number");
+    else
+        printf("will not take number");
+ 
+}
 

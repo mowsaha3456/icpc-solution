3dbf73c45fe173f1f10f4e12781ab2038978b257
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 17:41:49 +0600

    Update f.cpp

--- sheet 1/f.cpp
+++ sheet 1/f.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    long long N, M;
+    scanf("%lld %lld", &N, &M);
+ 
+    int last_N = N % 10;
+    int last_M = M % 10;
+ 
+    printf("%d\n", last_N + last_M);
+ 
+    return 0;
+}
 

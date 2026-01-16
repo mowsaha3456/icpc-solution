e1b3dbae89892c53852af746696145c825adfca1
mowsaha3456 <momosaha966@gmail.com>
2026-01-16 19:06:10 +0600

    Update j.cpp

--- sheet 7/j.cpp
+++ sheet 7/j.cpp
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+void num(int i, int n, long long int  multi)
+{
+    if(i>n)
+        {
+ 
+   printf("%lld", multi);}
+   else
+  num(i+1, n, multi*i);
+ 
+}
+int main(){
+int n;
+scanf("%d",&n);
+num(1 , n, 1);
+}
